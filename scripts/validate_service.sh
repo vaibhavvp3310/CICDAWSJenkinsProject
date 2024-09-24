cb47198479285866490ec2239b263bcdbd36276c
vaibhavvp3310 <vaibhavvp3310@gmail.com>
2024-09-24 18:06:42 -0400

    fifth

--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+
 echo "Validating the service"
-if curl -s http://localhost | grep "Welcome to my app"; then
+if curl -s http://localhost | grep "Register"; then
   echo "App is running successfully"
 else
   echo "App failed to start"
   exit 1
-fi  
+fi
